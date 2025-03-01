151d945ce45f31970ea7fa1922114ba19e938fae
Shivam <shivamfeb2101@gmail.com>
2025-03-01 21:23:00 +0530

    Socket server created to add multiple client cross interaction

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
-#include "Storage.h"
+#include "storage.h"
+#include "server.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    Storage storage;
+    storage storage;
     storage.create_key("name", "Alice");
     storage.read_key("name");
     storage.update_key("name", "Bob");
     storage.read_key("name");
     storage.delete_key("name");
     storage.read_key("name");
+    int port = 12345;
+    StartServer(port);
     return 0;
 }
---
+++ storage.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include "storage.h"
+#include <thread>
+
+using namespace std;
+
+string storage::create_key(const std::string &key, const std::string &value, int ttl) {
+  std::lock_guard<std::mutex> lock(mutex);
+  cache[key] = value;
+  return "Key got created";
+
+  if (ttl > 0) {
+    ttlMap[key] = std::chrono::steady_clock::now() + std::chrono::seconds(ttl);
+  }
+}
+
+std::string storage::read_key(const std::string &key) {
+  std::lock_guard<std::mutex> lock(mutex);
+  expire_keys();
+
+  if (cache.find(key) != cache.end()) {
+    cout << "Key: " << key << " Expired: " << cache[key] << endl;
+    return cache[key];
+  }
+  cout << "Key: " << key << " Not found" << endl;
+  return "Key not found";
+}
+
+string storage::update_key(const std::string &key, const std::string &value) {
+  std::lock_guard<std::mutex> lock(mutex);
+  if (cache.find(key) != cache.end()) {
+    cache[key] = value;
+    return "Key updated";
+  } else {
+    return "Key does not exist";
+  }
+}
+
+string storage::delete_key(const std::string &key) {
+  std::lock_guard<std::mutex> lock(mutex);
+  cache.erase(key);
+  ttlMap.erase(key);
+  return "Key deleted";
+}
+
+void storage::expire_keys() {
+  auto now = std::chrono::steady_clock::now();
+  for (auto it = ttlMap.begin(); it != ttlMap.end();) {
+    if (now >= it->second) {
+      cache.erase(it->first);
+      it = ttlMap.erase(it);
+    } else {
+      ++it;
+    }
+  }
+}
+
+// Queue operations
+void storage::enqueue(const std::string &value) {
+  std::lock_guard<std::mutex> lock(mutex);
+  q.push(value);
+}
+
+std::string storage::dequeue() {
+  std::lock_guard<std::mutex> lock(mutex);
+  if (q.empty()) return "Queue is empty";
+  std::string front = q.front();
+  q.pop();
+  return front;
+}
